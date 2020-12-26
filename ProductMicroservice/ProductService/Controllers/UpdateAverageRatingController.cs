1d9cdcd7286cb58944b14f24d97e7c39dfe5fc84
rifahtasnia <rifahtasnia@iut-dhaka.edu>
2020-12-27 00:31:19 +0600

    UpdateAverageRating Controller Added

---
+++ ProductMicroservice/ProductService/Controllers/UpdateAverageRatingController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.Database;
+using ProductService.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Controllers
+{
+    public class UpdateAverageRatingController : Controller
+    {
+        DatabaseContext db;
+        public UpdateAverageRatingController()
+        {
+            db = new DatabaseContext();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateAverageRating([FromBody] Product model)
+        {
+            try
+            {
+                Product myProduct = db.Products.Find(model.ProductID);
+                myProduct.AverageRating = model.AverageRating;
+                myProduct.NumberOfRaters = model.NumberOfRaters;
+                db.SaveChanges();
+                return StatusCode(StatusCodes.Status202Accepted);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, ex);
+            }
+        }
+    }
+}
